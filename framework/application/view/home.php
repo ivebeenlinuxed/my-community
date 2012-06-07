2f1c3b466146faa41143bb9040b75c706d41d1f1
Philip John <phil@philipjohn.co.uk>
2012-06-07 13:15:15 +0100

    Improvements to home page event display

--- framework/application/view/home.php
+++ framework/application/view/home.php
@@ -1,114 +1,133 @@
-		<div id="content" class="span8">
-			<div id="events" class="section">
-				<h2>Events</h2>
-				<?php
-					$events = \Model\Event::getAll();
-				?>
-				<table>
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Description</th>
-							<th>&nbsp;</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-							for ($i=1; $i<=6; $i++) {
-								$o = $events[$i];
-						?>
-						<tr>
-							<td><?php echo $o->title; ?></td>
-							<td><?php echo $o->description; ?></td>
-							<td><a href="/event/<?php echo $o->id ?>" class="read-more">Read More</a></td>
-						</tr>
-						<?php 
-							}
-						?>
-					</tbody>
-				</table>
-			</div>
-	
-	
-	
-			<h2>Organisations</h2>
-			<div id="organisations" class="section">
-				<div>
-					<div>
-						<section>
+		<div id="content" class="row">
+			<article class="span8">
+				<div id="events" class="section">
+					<h2>Events</h2>
+					<?php
+						$events = \Model\Event::getAll();
+					?>
+					<table class="table table-striped">
+						<caption>Upcoming events in Cheshire West and Chester</caption>
+						<thead>
+							<tr>
+								<th style="width:30%">Date and time</th>
+								<th style="width:70%">Title</th>
+							</tr>
+						</thead>
+						<tbody>
 							<?php
-							$orgs = \Model\Group::getAll();
+								for ($i=1; $i<=42; $i++) {
+									$o = $events[$i];
+									//var_dump($o);
 							?>
-							<div class="main_item">
-								<article>
-									<img src="/uploads/group/<?php echo $orgs[0]->id ?>_profile_pic.jpg" />
-									<h2><?php echo $orgs[0]->name ?></h2>
-									<div><?php echo $orgs[0]->description ?></div>
-									<a href="/group/<?php echo $orgs[0]->id ?>" class="read-more">Read More</a>
-								</article>
-								<div></div>
-							</div>
-							<?php
-							for ($i=1; $i<=6; $i++) {
-								$o = $orgs[$i];
-								?>
-							<div class="secondary_item">
-								<article>
-									<img src="/uploads/group/<?php echo $o->id ?>_profile_pic.jpg" />
-									<h2><?php echo $o->name ?></h2>
-									<div><?php echo $o->description ?></div>
-									<a href="/group/<?php echo $o->id ?>" class="read-more">Read More</a>
-								</article>
-							</div>
+							<tr>
+								<td><?php
+									date_default_timezone_set('Europe/London');
+									if ($o->FREQ == "WEEKLY"){
+										$wkdays = array('SU'=>'Sun', 'MO'=>'Mon', 'TU'=>'Tue', 'WE'=>'Wed', 'TH'=>'Thu', 'FR'=>'Fri', 'SA'=>'Sat');
+										echo "Each ".date('l', strtotime($wkdays[$o->BYDAY]))." at ".date('h:ia', $o->DTSTART);
+									}
+									else if ($o->FREQ == "DAILY"){
+										echo "Every day at ".date('h:ia', $o->DTSTART);
+									}
+								 ?></td>
+								<td><?php
+									echo '<a href="/event/'.$o->id.'">';
+									echo ($o->title == "") ? substr($o->description, 0, 50).'</a>...' : $o->title.'</a>';
+								?></td>
+							</tr>
 							<?php 
-							}
+								}
 							?>
-							
-							<div class="clear"></div>
-						</section>
+						</tbody>
+					</table>
+				</div>
+		
+		
+		
+				<h2>Organisations</h2>
+				<div id="organisations" class="section">
+					<div>
+						<div>
+							<section>
+								<?php
+								$orgs = \Model\Group::getAll();
+								?>
+								<div class="main_item">
+									<article>
+										<img src="/uploads/group/<?php echo $orgs[0]->id ?>_profile_pic.jpg" />
+										<h2><?php echo $orgs[0]->name ?></h2>
+										<div><?php echo $orgs[0]->description ?></div>
+										<a href="/group/<?php echo $orgs[0]->id ?>" class="read-more">Read More</a>
+									</article>
+									<div></div>
+								</div>
+								<?php
+								for ($i=1; $i<=6; $i++) {
+									$o = $orgs[$i];
+									?>
+								<div class="secondary_item">
+									<article>
+										<img src="/uploads/group/<?php echo $o->id ?>_profile_pic.jpg" />
+										<h2><?php echo $o->name ?></h2>
+										<div><?php echo $o->description ?></div>
+										<a href="/group/<?php echo $o->id ?>" class="read-more">Read More</a>
+									</article>
+								</div>
+								<?php 
+								}
+								?>
+								
+								<div class="clear"></div>
+							</section>
+						</div>
 					</div>
 				</div>
-			</div>
-	
-	
-	
-			<h2>Venues</h2>
-			<div id="venues" class="section">
-				<div>
+		
+		
+		
+				<h2>Venues</h2>
+				<div id="venues" class="section">
 					<div>
-						<section>
-							<?php
-							$venues = \Model\Venue::getAll();
-							?>
-							<div class="main_item">
-								<article>
-									<img src="/uploads/group/<?php echo $venues[0]->id ?>_profile_pic.jpg" />
-									<h2><?php echo $venues[0]->name ?></h2>
-									<div><?php echo $venues[0]->description ?></div>
-									<a href="/group/<?php echo $venues[0]->id ?>" class="read-more">Read More</a>
-								</article>
-								<div></div>
-							</div>
-							<?php
-							for ($i=1; $i<=6; $i++) {
-								$o = $venues[$i];
+						<div>
+							<section>
+								<?php
+								$venues = \Model\Venue::getAll();
 								?>
-							<div class="secondary_item">
-								<article>
-									<img src="/uploads/group/<?php echo $o->id ?>_profile_pic.jpg" />
-									<h2><?php echo $o->name ?></h2>
-									<div><?php echo $o->description ?></div>
-									<a href="/group/<?php echo $o->id ?>" class="read-more">Read More</a>
-								</article>
-							</div>
-							<?php 
-							}
-							?>
-							
-							<div class="clear"></div>
-						</section>
+								<div class="main_item">
+									<article>
+										<img src="/uploads/group/<?php echo $venues[0]->id ?>_profile_pic.jpg" />
+										<h2><?php echo $venues[0]->name ?></h2>
+										<div><?php echo $venues[0]->description ?></div>
+										<a href="/group/<?php echo $venues[0]->id ?>" class="read-more">Read More</a>
+									</article>
+									<div></div>
+								</div>
+								<?php
+								for ($i=1; $i<=6; $i++) {
+									$o = $venues[$i];
+									?>
+								<div class="secondary_item">
+									<article>
+										<img src="/uploads/group/<?php echo $o->id ?>_profile_pic.jpg" />
+										<h2><?php echo $o->name ?></h2>
+										<div><?php echo $o->description ?></div>
+										<a href="/group/<?php echo $o->id ?>" class="read-more">Read More</a>
+									</article>
+								</div>
+								<?php 
+								}
+								?>
+								
+								<div class="clear"></div>
+							</section>
+						</div>
 					</div>
 				</div>
+			</article>
+			
+			<div id="sidebar" class="span4">
+				<h2>Sidebar</h2>
 			</div>
-		</div>
 
+		</div>
+		
