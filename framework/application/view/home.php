7b90650a650704b7315e9ef1b576d522bb2ec1c8
Philip John <phil@philipjohn.co.uk>
2012-06-20 16:23:17 +0100

    More tidying up of groups on home page

--- framework/application/view/home.php
+++ framework/application/view/home.php
@@ -8,12 +8,14 @@
 					
 				</div>
 		
-		
-		
 				<h2>Local Groups</h2>
 				<div id="organisations" class="section">
 					<div>
 						<div>
+							<form class="form-search">
+								<input type="text" class="input-medium search-query span7" placeholder="Enter your postcode or a place for groups near you">
+								<button type="submit" class="btn">Search</button>
+							</form>
 							<section class="row-fluid">
 								<?php
 								$orgs = \Model\Group::getAll();
@@ -21,7 +23,7 @@
 								for ($i=1; $i<=6; $i++) {
 									$o = $orgs[$i];
 									?>
-								<div class="org">
+								<div class="org well">
 									<img src="/uploads/group/<?php echo $orgs[0]->id ?>_profile_pic.jpg" width="70" />
 									<h2><?php echo $orgs[0]->name ?></h2>
 									<p><?php echo substr($orgs[0]->description, 0, 100); ?> <a href="/group/<?php echo $orgs[0]->id ?>" class="read-more">Read More</a></p>
